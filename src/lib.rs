d0d257893a1e723ec2bde50746eed7605a3fc18e
NickelAngeStudio <github@nickelange.studio>
2026-01-27 12:50:59 -0500

    Read write tcp. Fix #2

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,33 @@
+/* 
+Copyright (c) 2026  NickelAnge.Studio 
+Email               mathieu.grenier@nickelange.studio
+Git                 https://github.com/NickelAngeStudio/ethos-client
 
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 
 #[cfg(test)]
 mod tests {
     use std::io::prelude::*;
     use std::net::TcpStream;
+
+    use ethos_core::ETHOS_TCP_PORT;
 
     #[test]
     fn connection() {
@@ -14,9 +38,19 @@ mod tests {
     }
 
     fn create_connection()  -> std::io::Result<()> {
-        let mut stream = TcpStream::connect("127.0.0.1:3847")?;
+        let connect_string = format!("127.0.0.1:{}", ETHOS_TCP_PORT);
+        let buf =  &mut [0 as u8; 128];
+        let mut stream = TcpStream::connect(connect_string)?;
 
-        //stream.write(&[1])?;
+        loop {
+            buf[0] += 1;
+            if buf[0]>= 255{
+                buf[0] = 0;
+            }
+            stream.write(&buf[0..8])?;
+        }
+
+        
         //stream.read(&mut [0; 128])?;
 
         Ok(())
